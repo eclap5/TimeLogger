a5d98407dab77e0dbb44b6485671d253b754d37e
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2024-07-11 21:39:35 +0300

    separated task title and description in Log model

--- Models/Log.cs
+++ Models/Log.cs
@@ -10,7 +10,8 @@ namespace TimeLogger.Models
         public string? StartTime { get; set; }
         public string? EndTime { get; set; }
         public DateOnly Date { get; set; }
-        public string? Task { get; set; }
+        public string? TaskTitle { get; set; }
+        public string? TaskDescription { get; set; }
         public DayCategory DayCategory { get; set; }
     }
 }
