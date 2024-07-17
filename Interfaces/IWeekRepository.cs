fd767e2a945b5e7b99bda1f187147542d0f4dc65
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2024-07-17 23:00:20 +0300

    Added week repository and interface

---
+++ Interfaces/IWeekRepository.cs
@@ -0,0 +1,14 @@
+﻿using TimeLogger.Models;
+
+namespace TimeLogger.Interfaces
+{
+    public interface IWeekRepository
+    {
+        Task<IEnumerable<Week>> GetWeeksAsync();
+        Task<Week?> GetWeekByIdAsync(int id);
+        Task<bool> AddWeekAsync(Week week);
+        Task<bool> UpdateWeekAsync(Week week);
+        Task<bool> DeleteWeekAsync(Week week);
+        Task<bool> SaveAsync();
+    }
+}
---
+++ Repository/WeekRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeLogger.Data;
+using TimeLogger.Interfaces;
+using TimeLogger.Models;
+
+namespace TimeLogger.Repository
+{
+    public class WeekRepository(ApplicationDbContext context) : IWeekRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<bool> AddWeekAsync(Week week)
+        {
+            await _context.AddAsync(week);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteWeekAsync(Week week)
+        {
+            _context.Remove(week);
+            return await SaveAsync();
+        }
+
+        public async Task<Week?> GetWeekByIdAsync(int id)
+        {
+            return await _context.Weeks.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Week>> GetWeeksAsync()
+        {
+            return await _context.Weeks.ToListAsync();
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateWeekAsync(Week week)
+        {
+            _context.Update(week);
+            return await SaveAsync();
+        }
+    }
+}
