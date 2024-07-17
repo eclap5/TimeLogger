91bcbe0ed820af804292273ccb84a9481e9f41ab
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2024-07-17 21:57:49 +0300

    Added day interface and repository

---
+++ Interfaces/IDayRepository.cs
@@ -0,0 +1,15 @@
+﻿using TimeLogger.Models;
+
+namespace TimeLogger.Interfaces
+{
+    public interface IDayRepository
+    {
+        Task<IEnumerable<Day>> GetDaysAsync();
+        Task<IEnumerable<Day>> GetDaysByWeekIdAsync(int weekId);
+        Task<Day?> GetDayByIdAsync(int id);
+        Task<bool> AddDayAsync(Day day);
+        Task<bool> UpdateDayAsync(Day day);
+        Task<bool> DeleteDayAsync(Day day);
+        Task<bool> SaveAsync();
+    }
+}
---
+++ Repository/DayRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeLogger.Data;
+using TimeLogger.Interfaces;
+using TimeLogger.Models;
+
+namespace TimeLogger.Repository
+{
+    public class DayRepository(ApplicationDbContext context) : IDayRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<bool> AddDayAsync(Day day)
+        {
+            await _context.AddAsync(day);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteDayAsync(Day day)
+        {
+            _context.Remove(day);
+            return await SaveAsync();
+        }
+
+        public async Task<Day?> GetDayByIdAsync(int id)
+        {
+            return await _context.Days.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Day>> GetDaysAsync()
+        {
+            return await _context.Days.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Day>> GetDaysByWeekIdAsync(int weekId)
+        {
+            return await _context.Days.Where(d => d.WeekId == weekId).ToListAsync();
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateDayAsync(Day day)
+        {
+            _context.Update(day);
+            return await SaveAsync();
+        }
+    }
+}
