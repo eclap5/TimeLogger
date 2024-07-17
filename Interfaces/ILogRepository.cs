bce054682a5c3046938570ca6f52654835d04b1f
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2024-07-17 21:30:03 +0300

    Added Log interface and repository

---
+++ Interfaces/ILogRepository.cs
@@ -0,0 +1,16 @@
+﻿using TimeLogger.Models;
+
+namespace TimeLogger.Interfaces
+{
+    public interface ILogRepository
+    {
+        Task<IEnumerable<Log>> GetLogsAsync();
+        Task<IEnumerable<Log>> GetLogsByDayIdAsync(int dayId);
+        Task<IEnumerable<Log>> GetLogsByWeekIdAsync(int weekId);
+        Task<Log?> GetLogByIdAsync(int id);
+        Task<bool> AddLogAsync(Log log);
+        Task<bool> UpdateLogAsync(Log log);
+        Task<bool> DeleteLogAsync(Log log);
+        Task<bool> SaveAsync();
+    }
+}
---
+++ Repository/LogRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeLogger.Data;
+using TimeLogger.Interfaces;
+using TimeLogger.Models;
+
+namespace TimeLogger.Repository
+{
+    public class LogRepository(ApplicationDbContext context) : ILogRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<bool> AddLogAsync(Log log)
+        {
+            await _context.AddAsync(log);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteLogAsync(Log log)
+        {
+            _context.Remove(log);
+            return await SaveAsync();
+        }
+
+        public async Task<Log?> GetLogByIdAsync(int id)
+        {
+            return await _context.Logs.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Log>> GetLogsAsync()
+        {
+            return await _context.Logs.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Log>> GetLogsByDayIdAsync(int dayId)
+        {
+            return await _context.Logs.Where(d => d.Id == dayId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Log>> GetLogsByWeekIdAsync(int weekId)
+        {
+            return await _context.Logs.Include(log => log.Day).Where(log => log.Day != null && log.Day.WeekId == weekId).ToListAsync();
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public Task<bool> UpdateLogAsync(Log log)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
