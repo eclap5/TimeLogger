e3471ff5e6f177f02542363e4fc0591e933bec3b
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2024-08-07 01:52:00 +0300

    Added SearchTerm functionality

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimeLogger.Interfaces;
+using TimeLogger.ViewModels;
+
+namespace TimeLogger.Controllers
+{
+    public class SearchController(ILogRepository logRepository, IDayRepository dayRepository, IWeekRepository weekRepository) : Controller
+    {
+        private readonly ILogRepository _logRepository = logRepository;
+        private readonly IDayRepository _dayRepository = dayRepository;
+        private readonly IWeekRepository _weekRepository = weekRepository;
+
+        public IActionResult Index()
+        {
+            SearchViewModel searchViewModel = GetSearchViewModel();
+            return View(searchViewModel);
+        }
+
+        public IActionResult SearchTerm(SearchViewModel searchViewModel)
+        {
+            if (searchViewModel.IsDateSearch)
+            {
+                searchViewModel.SearchTermText = "By date";
+            }
+            else if (searchViewModel.IsWeekSearch)
+            {
+                searchViewModel.SearchTermText = "By week";
+            }
+
+            return View("Index", searchViewModel);
+        }
+
+        private static SearchViewModel GetSearchViewModel()
+        {
+            SearchViewModel searchViewModel = new()
+            {
+                IsDateSearch = false,
+                IsWeekSearch = false
+            };
+
+            return searchViewModel;
+        }
+    }
+}
---
+++ ViewModels/SearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeLogger.ViewModels
+{
+    public class SearchViewModel
+    {
+        public bool IsDateSearch { get; set; }
+        public bool IsWeekSearch { get; set; }
+        public string SearchTermText { get; set; } = "Search term";
+    }
+}
