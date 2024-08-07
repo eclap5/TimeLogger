6cc8edaa9a96ff71918cde19a7bb577fe8be7119
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2024-08-07 17:35:21 +0300

    Finalized search term input fields

--- ViewModels/SearchViewModel.cs
+++ ViewModels/SearchViewModel.cs
@@ -2,8 +2,14 @@
 {
     public class SearchViewModel
     {
+        // Search term properties
         public bool IsDateSearch { get; set; }
         public bool IsWeekSearch { get; set; }
         public string SearchTermText { get; set; } = "Search term";
+        
+        // Search properties
+        public DateOnly? Date { get; set; }
+        public int? WeekNumber { get; set; }
+        public int? Year { get; set; }
     }
 }
